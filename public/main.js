510746433086cefc367256818432a19e5ada329f
Harry Francis <harryad3@gmail.com>
2023-05-29 19:07:04 +0100

    adding discounts logic

--- public/main.js
+++ public/main.js
@@ -3,19 +3,22 @@ class CheckoutReceipt extends HTMLElement {
         super();
 
         // Data
+        // Could be added with UI
         this.basket = [
-            { id: '1' },
-            { id: '1' },
-            { id: '1' },
-            { id: '1' },
-            { id: '2' },
-            { id: '2' },
-            { id: '2' },
-            { id: '3', weight: 0.2 },
-            { id: '4' },
-            { id: '5' },
-            { id: '6' },
-            { id: '7' }
+            { id: 1 },
+            { id: 1 },
+            { id: 1 },
+            { id: 1 },
+            { id: 1 },
+            { id: 2 },
+            { id: 2 },
+            { id: 2 },
+            { id: 2 },
+            { id: 3, weight: 0.2 },
+            { id: 4 },
+            { id: 5 },
+            { id: 6 },
+            { id: 7 }
         ]
 
         this.item_data = [
@@ -23,9 +26,28 @@ class CheckoutReceipt extends HTMLElement {
             { id: 2, title: 'Cola', price: 0.70 },
             { id: 3, title: 'Onions', price: 1.99 },
             { id: 4, title: 'Ale - Butty Bach', price: 2.49 },
-            { id: 5, title: 'Ale - Landlord', price: 2.49 },
-            { id: 6, title: 'Ale - Doombar', price: 2.49 },
-            { id: 7, title: 'Ale - Guiness', price: 2.49 }
+            { id: 5, title: 'Ale - Landlord', price: 2.29 },
+            { id: 6, title: 'Ale - Doombar', price: 2.39 },
+            { id: 7, title: 'Ale - Guiness', price: 2.19 }
+        ]
+
+
+        // Update basket with item_data
+        this.basket.forEach(item => {
+            let item_data = this.item_data.find(data => data.id == item.id)
+
+            item.title = item_data.title
+            item.price = parseFloat((item.weight ? (item_data.price * item.weight) : item_data.price).toFixed(2))
+            item.price_per_kg = item_data.price
+        })
+
+        this.discount_bxgxf = [
+            { title: 'Beans 3 for 2', trigger_products: [1], trigger_quantity: 3, charge_quantity: 2 }
+        ]
+
+        this.discount_set_quantity_set_price = [
+            { title: 'Cola 2 for £1', trigger_products: [2], trigger_quantity: 2, charge_amount: 1 },
+            { title: 'Ales 3 for £6', trigger_products: [4, 5, 6, 7], trigger_quantity: 3, charge_amount: 6 }
         ]
 
         // Elements
@@ -37,6 +59,7 @@ class CheckoutReceipt extends HTMLElement {
 
         // Values 
         this.subtotal = 0
+        this.savings_total = 0
 
         // Markup
         this.item_markup = `
@@ -52,45 +75,100 @@ class CheckoutReceipt extends HTMLElement {
     addItems() {
         // Foreach item in basket
         this.basket.forEach(item => {
-            // Find associated item data
-            let item_data = this.item_data.find(data => data.id == item.id)
+            // Create markup and replace values
+            let formatted_markup = this.item_markup
+                .replace('{{ item_title }}', `${item.title} ${(item.weight ? ` <em>(${item.weight}kg @ £${item.price_per_kg}/kg)</em>` : '')}`)
+                .replace('{{ item_value }}', `£${item.price.toFixed(2)}`)
 
-            // Create markup with data
-            if (item.weight) {
-                // For items with weight, we use two lines
-                let formatted_markup_1 = this.item_markup
-                    .replace('{{ item_title }}', item_data.title)
-                    .replace('{{ item_value }}', '')                
+            // Append item to list
+            this.el_item_list.insertAdjacentHTML('beforeEnd', formatted_markup)
 
-                let formatted_markup_2 = this.item_markup
-                    .replace('{{ item_title }}', `${item.weight.toFixed(3)}kg @ £${item_data.price.toFixed(2)}`)
-                    .replace('{{ item_value }}', `£${(item_data.price * item.weight).toFixed(2)}`)
-
-                // Append item to list
-                this.el_item_list.insertAdjacentHTML('beforeEnd', formatted_markup_1)
-                this.el_item_list.insertAdjacentHTML('beforeEnd', formatted_markup_2)
-
-                // Update subtotal
-                this.subtotal += parseFloat((item_data.price * item.weight).toFixed(2))
-            } else {
-                let formatted_markup = this.item_markup
-                    .replace('{{ item_title }}', item_data.title)
-                    .replace('{{ item_value }}', `£${item_data.price.toFixed(2)}`)
-                
-                // Append item to list
-                this.el_item_list.insertAdjacentHTML('beforeEnd', formatted_markup)
-
-                this.subtotal += parseFloat(item_data.price.toFixed(2))
-            }
+            // Update subtotal
+            this.subtotal += parseFloat(item.price)
         })
-
-        // Update subtotal
-        this.el_subtotal.innerHTML = `£${this.subtotal}`
     }
 
+    addDiscountBxgxf() {
+        this.discount_bxgxf.forEach(discount => {
+            // Find each qualifiable item in the basket
+            let qualifiable_items = this.basket.filter(item => discount.trigger_products.includes(item.id))
+
+            // Order items (cheapest first)
+            qualifiable_items.sort(function(a, b){return a.price - b.price});
+
+            // Remove items that wont fit into discount
+            let items_to_discount = qualifiable_items.slice(0, Math.floor(qualifiable_items.length / discount.trigger_quantity));
+
+            // Calculate savings amount
+            let savings_amount = parseFloat(items_to_discount.reduce((acc, item) => acc + parseFloat(item.price), 0))
+
+            // Create markup and replace values
+            let formatted_markup = this.item_markup
+                .replace('{{ item_title }}', discount.title)
+                .replace('{{ item_value }}', `£${savings_amount.toFixed(2)}`)
+
+            // Append item to list
+            this.el_savings_list.insertAdjacentHTML('beforeEnd', formatted_markup)
+
+            // Update savings amount
+            this.savings_total += parseFloat(savings_amount)
+        })
+    }
+
+    addDiscountSetQuantityPrice() {
+        this.discount_set_quantity_set_price.forEach(discount => {
+            let discount_savings = 0;
+
+            // Find each qualifiable item in the basket
+            let qualifiable_items = this.basket.filter(item => discount.trigger_products.includes(item.id))
+         
+            // Order items (cheapest first)
+            qualifiable_items.sort((a, b) => a.price - b.price);
+
+            // Remove items that wont fit into discount
+            let items_to_discount = qualifiable_items.slice(0, (qualifiable_items.length - (qualifiable_items.length % discount.trigger_quantity)));
+
+            // Put products into groups based on trigger quantities
+            let groups = items_to_discount.map((item, index) => {
+                return index % discount.trigger_quantity === 0 ? items_to_discount.slice(index, index + discount.trigger_quantity) : null
+            }).filter(item => item)
+
+            groups.forEach(group => {
+                // Calculate group cost
+                let group_cost = parseFloat(group.reduce((acc, item) => acc + parseFloat(item.price), 0))
+                
+                // Calculate group savings
+                let group_savings = group_cost - discount.charge_amount
+                
+                discount_savings += parseFloat(group_savings)
+            })
+
+            // Create markup and replace values
+            let formatted_markup = this.item_markup
+                .replace('{{ item_title }}', discount.title)
+                .replace('{{ item_value }}', `£${discount_savings.toFixed(2)}`)
+
+            // Append item to list
+            this.el_savings_list.insertAdjacentHTML('beforeEnd', formatted_markup)
+
+            // Update savings amount
+            this.savings_total += parseFloat(discount_savings)
+        })
+    }
+    
     init() {
         this.addItems()
-        // addTotals
+        this.addDiscountBxgxf()
+        this.addDiscountSetQuantityPrice()
+        
+        // Update subtotal markup
+        this.el_subtotal.innerHTML = `£${this.subtotal.toFixed(2)}`
+
+        // Update total savings markup
+        this.el_savings_total.innerHTML = `£${this.savings_total.toFixed(2)}`
+
+        // Update total markup
+        this.el_receipt_total.innerHTML = `£${(this.subtotal - this.savings_total).toFixed(2)}`
     }
 }
   
